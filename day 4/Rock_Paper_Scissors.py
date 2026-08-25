934deb2adfc04e76ffd84cc65d24ed67fdda42d3
Snowchild0159 <mhmdashraf0159@gmail.com>
2026-08-25 08:34:19 +0300

    day4 enhancment

--- day 4/Rock_Paper_Scissors.py
+++ day 4/Rock_Paper_Scissors.py
@@ -36,37 +36,39 @@ Scissors = ('''
 
 Game = [ Rock , Paper , Scissors]
 
-choice = input("what do you choose? type 0 for Rock , 1 for Paper , 2 for Scissors \n")
-int_choice = int(choice)
-your_choice = Game[int_choice]
-computer_choice = random.choice(Game)
-print(f"your choice is \n {your_choice}")
-print(f"computer choice is \n {computer_choice}")
-if your_choice == computer_choice : 
-    print("it is draw !!!")
+choice = int(input("what do you choose? type 0 for Rock , 1 for Paper , 2 for Scissors \n"))
+if choice > 2 :
+    print("you choose unvalid number , you loose")
+else :
+    your_choice = Game[choice]
+    computer_choice = random.choice(Game)
+    print(f"your choice is \n {your_choice}")
+    print(f"computer choice is \n {computer_choice}")
+    if your_choice == computer_choice : 
+        print("it is draw !!!")
 
-#easier solution : 
-else:
-    if your_choice == Rock and computer_choice == Scissors or your_choice == Scissors and computer_choice == Paper or your_choice == Paper and computer_choice == Rock:
-        print("you  win!!!")
+    #easier solution : 
     
+    elif your_choice == Rock and computer_choice == Scissors or your_choice == Scissors and computer_choice == Paper or your_choice == Paper and computer_choice == Rock:
+            print("you  win!!!")
+        
     else:
-        print("computer wins :(")
+            print("computer wins :(")
 
 
-#another solution : 
- # if your_choice == Rock and computer_choice == Scissors :
-    #     print("you  win!!!")
-    # elif your_choice == Rock and computer_choice == Paper :
-    #     print("computer wins :(")
-    # elif your_choice == Scissors and computer_choice == Paper :
-    #     print("you  win!!!")
-    # elif your_choice == Paper and computer_choice == Scissors :
-    #     print("computer wins :(")
-    # elif your_choice == Paper and computer_choice == Rock :
-    #     print("you  win!!!")
-    # else:
-    #     print("computer wins :(")
+    #another solution : 
+    # if your_choice == Rock and computer_choice == Scissors :
+        #     print("you  win!!!")
+        # elif your_choice == Rock and computer_choice == Paper :
+        #     print("computer wins :(")
+        # elif your_choice == Scissors and computer_choice == Paper :
+        #     print("you  win!!!")
+        # elif your_choice == Paper and computer_choice == Scissors :
+        #     print("computer wins :(")
+        # elif your_choice == Paper and computer_choice == Rock :
+        #     print("you  win!!!")
+        # else:
+        #     print("computer wins :(")
 
 
 
